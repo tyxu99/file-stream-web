1e6b98be9d2ca979aeaace57bc9e8485a250ea3f
tyxu99 <uasic@outlook.com>
2023-04-21 17:21:22 +0800

    feat: init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,34 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import fetcher from "@/utils/fetcher";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const fileUpload = (e: any) => {
+    console.log(e.target.files[0]);
+    if (e.target.files[0]) {
+      const file: File = e.target.files[0];
+      const stream = file.stream();
+      const filename = file.name;
+      console.log("filename", filename, stream);
+      const { signal } = new AbortController();
+
+      fetcher("http://localhost:3001/api/files/fileStreamUpload", {
+        method: "POST",
+        // headers: {
+        //   Accept: "application/json",
+        //   "Content-Type": "application/octet-stream",
+        // },
+        signal,
+        body: stream,
+        duplex: "half",
+      }).then((res) => console.log("======", res));
+    }
+  };
+
   return (
     <>
       <Head>
@@ -15,100 +38,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <input type="file" onChange={fileUpload} />
       </main>
     </>
-  )
+  );
 }
---
+++ src/utils/fetcher.ts
@@ -0,0 +1,44 @@
+const defaults = {
+  mode: "cors" as const,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+};
+
+const fetcher = async (url: string, config: any = null) => {
+  const conf = config ? { ...defaults, ...config } : { ...defaults, method: "GET" };
+
+  fetch(url, conf)
+    .then((response) => {
+      if (!response.ok) throw response;
+
+      const [contentType, contentDisposition] = [
+        response.headers.get("content-type"),
+        response.headers.get("content-disposition"),
+      ];
+
+      const result =
+        contentType &&
+        (contentType?.indexOf("application/json") !== -1 ||
+          contentType?.indexOf("text/plain") !== -1)
+          ? response.json()
+          : contentDisposition?.indexOf("attachment") !== -1
+          ? response.blob()
+          : response;
+
+      console.log(response);
+      return response.json();
+    })
+    .catch(async (error) => {
+      // const contentType = error.headers.get("content-type");
+      // const errResult =
+      //   contentType && contentType?.indexOf("application/problem+json") !== -1
+      //     ? await error.json()
+      //     : error;
+      console.log("catch", error);
+      throw error;
+    });
+};
+
+export default fetcher;
