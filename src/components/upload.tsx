ade06b9fcda087a04f60d98cf1e3c5e9dc4a7ae7
tyxu99 <uasic@outlook.com>
2023-05-23 17:55:42 +0800

    feat: upload

--- src/components/upload.tsx
+++ src/components/upload.tsx
@@ -1,32 +1,49 @@
-import { Button, Upload } from "antd";
+import { Button, Upload, Progress } from "antd";
+import { useState } from "react";
 
 const Index = () => {
-  const onBeforeUpload = (file: any) => {
-    const fd = new FormData();
-    fd.append("name", file.name);
-    fd.append("current", "5");
-    fd.append("file", file);
-    console.log(fd);
+  const [percent, setPercent] = useState(0);
+  const [acObj, setAcObj] = useState<any>(null);
 
-    fetch("http://127.0.0.1:8888/api/upload", {
-      method: "POST",
-      body: fd,
-    })
-      .then((res) => {
-        if (res.ok) {
-          console.log("success");
-          return res.json();
-        } else {
-          console.log("error", res);
-        }
-      })
-      .then((res) => {
-        console.log("res is", res);
-      });
+  const isFileExist = async (file: File) => {
+    const fr = new FileReader();
+    fr.readAsArrayBuffer(file);
+    fr.onload = () => {
+      crypto.subtle
+        .digest("SHA-256", fr.result as BufferSource)
+        .then(async (res) => {
+          const hashArray = Array.from(new Uint8Array(res)); // 将 ArrayBuffer 转换为 Uint8Array
+          const hashBase64 = btoa(
+            hashArray.reduce(
+              (data, byte) => data + String.fromCharCode(byte),
+              "",
+            ),
+          ).replace(/\/|\\/g, ""); // 将 Uint8Array 转换为 Base64 字符串
+          console.log("File hash: ", hashBase64);
+          try {
+            const res = await fetch(
+              "http://127.0.0.1:8888/api/isFileExist?filename=" +
+                hashBase64 +
+                file.name.slice(file.name.lastIndexOf(".")),
+            );
+            if (!res.ok) {
+              throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            const { data } = await res.json();
+            if (data) {
+              console.log("file exist");
+            } else {
+              uploadChunks(file, hashBase64);
+            }
+          } catch (err) {
+            console.log(err);
+          }
+        });
+    };
   };
 
-  const uploadChunks = async (file: File) => {
-    const CHUNK_SIZE = 2 * 1024 * 1024; // 10MB
+  const uploadChunks = async (file: File, filename: string) => {
+    const CHUNK_SIZE = 1024 * 1024 * 4;
     const fileSize = file.size;
 
     const chunks = Math.ceil(fileSize / CHUNK_SIZE);
@@ -36,35 +53,62 @@ const Index = () => {
       const chunk = file.slice(start, start + CHUNK_SIZE);
       const formData = new FormData();
       formData.append("file", chunk);
-      formData.append("filename", file.name);
+      formData.append("filename", filename);
       formData.append("current", chunkIndex + "");
+      const ac = new AbortController();
+      setAcObj(ac);
 
       try {
         const res = await fetch("http://127.0.0.1:8888/api/uploadHugeFile", {
           method: "POST",
           body: formData,
+          signal: ac.signal,
         });
         if (!res.ok) {
           throw new Error(`HTTP error! status: ${res.status}`);
         }
         start += CHUNK_SIZE;
         chunkIndex++;
+
+        console.log((chunkIndex / chunks) * 100);
+        setPercent((chunkIndex / chunks) * 100);
       } catch (error) {
         console.log("Error uploading chunk", error);
         throw error;
       }
     }
-    console.log("chunkIndex", chunkIndex, chunks);
+    mergeChunks(filename + file.name.slice(file.name.lastIndexOf(".")));
   };
+
+  const mergeChunks = async (filename: string) => {
+    try {
+      const res = await fetch(
+        "http://127.0.0.1:8888/api/mergeFileChunks?filename=" + filename,
+      );
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      const data = await res.json();
+      console.log("data", data);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const pauseUpload = () => {
+    acObj && acObj.abort();
+  };
+
+  const recoverUpload = () => {};
 
   return (
     <>
-      <Upload beforeUpload={onBeforeUpload} showUploadList={false}>
-        <Button type="primary">上传文件</Button>
-      </Upload>
-      <Upload beforeUpload={uploadChunks} showUploadList={false}>
+      <Upload beforeUpload={isFileExist} showUploadList={false}>
         <Button type="primary">大文件分片</Button>
       </Upload>
+      <Button onClick={pauseUpload}>暂停</Button>
+      <Button onClick={recoverUpload}>恢复</Button>
+      <Progress status="active" percent={percent} />
     </>
   );
 };
