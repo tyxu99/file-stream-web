c3ece9b2fd0ff3e262321ed199aa1e4247349010
tyxu99 <uasic@outlook.com>
2023-06-16 16:14:13 +0800

    feat: unknown

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,39 @@
 import GenerateFileId from "@/components/generateFileId";
 import Upload from "@/components/upload";
+import { Observable } from "rxjs";
 
 const Home = () => {
-  const onFileChange = (file: any) => {
-    console.log(file);
+  const onRxObservable = () => {
+    const observable = new Observable((subscriber) => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+
+    console.log("before subscribe");
+
+    observable.subscribe({
+      next(x) {
+        console.log("get value", x);
+      },
+      error(err) {
+        console.log("sth wrong", err);
+      },
+      complete() {
+        console.log("done");
+      },
+    });
+
+    console.log("after subscribe");
   };
 
   return (
     <>
+      <div onClick={onRxObservable}>Observable</div>
       <GenerateFileId />
       <Upload />
     </>
