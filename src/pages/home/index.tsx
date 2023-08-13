edf476d74ea4189db360137e8eda9803565a7420
tyxu99 <uasic@outlook.com>
2023-08-14 00:31:15 +0800

    feat: jsonToExcel

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,7 @@ import Upload from "@/components/upload";
 import { Observable } from "rxjs";
 // import NativeDrag from "@/components/NativeDrag";
 import SvgClip from "@/components/SvgClip";
+import * as XLSX from "xlsx";
 
 const Home = () => {
   const onRxObservable = () => {
@@ -31,6 +32,42 @@ const Home = () => {
     });
 
     console.log("after subscribe");
+  };
+
+  const data = new Array(5).fill({
+    name: 1,
+    age: 12,
+    sex: 1,
+    addr: "asdfasd",
+    fate: "dead",
+    now: new Date(),
+    drink: "wine",
+  });
+
+  const jsonToExcel = () => {
+    const t = data.map((d) => ({
+      ...d,
+      name: d.name + 1,
+      sex: d.sex === 1 ? "男" : "女",
+      fate: d.fate === "dead" ? "死亡" : "未知",
+      now: d.now.toISOString(),
+    }));
+    const sheet = XLSX.utils.json_to_sheet(t);
+    // ["A1", "B1", "C1", "D1", "E1", "F1", "G1"].forEach((d) => {
+    //   sheet[d]["v"] = obj[sheet[d]["v"]];
+    // });
+
+    sheet["!cols"] = Object.values(t[0]).map((d) => ({
+      wch: (d + "").length + 5,
+    }));
+    XLSX.utils.sheet_add_aoa(
+      sheet,
+      [["姓名", "年龄", "性别", "地址", "命运", "当前时间", "饮料"]],
+      { origin: "A1" },
+    );
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, sheet, "sheet1");
+    XLSX.writeFile(workbook, "test.xlsx");
   };
 
   return (
