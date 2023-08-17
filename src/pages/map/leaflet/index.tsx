e870b1da804f09f6328a5edc6a5e6305585b40ef
tyxu99 <uasic@outlook.com>
2023-08-17 17:58:33 +0800

    feat: leafletPathNav

--- src/pages/map/leaflet/index.tsx
+++ src/pages/map/leaflet/index.tsx
@@ -10,37 +10,7 @@ const DynamicMapContainer = dynamic(
 );
 
 const Index = () => {
-  const [loading, setLoading] = useState(true);
-  const [geoLocation, setGeoLocation] = useState({
-    longitude: 31.2304,
-    latitude: 121.4737,
-  });
-
-  useEffect(() => {
-    if (navigator) {
-      setLoading(false);
-      navigator.geolocation.getCurrentPosition(
-        ({ coords }) => {
-          const { longitude, latitude } = coords;
-          console.log(coords);
-          setGeoLocation({ longitude, latitude });
-        },
-        (err) => {
-          console.log("error", err);
-        },
-      );
-    }
-  }, []);
-
-  return (
-    <>
-      {loading ? (
-        <div>leaflet loading...</div>
-      ) : (
-        <DynamicMapContainer {...geoLocation} />
-      )}
-    </>
-  );
+  return <DynamicMapContainer />;
 };
 
 export default Index;
